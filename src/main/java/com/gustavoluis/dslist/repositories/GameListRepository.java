d830cc11c0942522532c68e0c9bd8e29f93bd557
LuisGustavoDev <luisgustavodev@hotmail.com>
2024-11-12 22:32:17 -0300

    GET lists

---
+++ src/main/java/com/gustavoluis/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.gustavoluis.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gustavoluis.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+    
+}
